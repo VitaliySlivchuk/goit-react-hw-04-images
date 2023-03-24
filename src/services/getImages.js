c3a69a16c56b423b0f62699a5a2728594f4b5971
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2023-03-24 20:32:30 +0200

    Update getImages.js

--- src/services/getImages.js
+++ src/services/getImages.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '32799595-4883f30010edd47462b21129f';
-const config = '&image_type=photo&orientation=horizontal&per_page=3';
+const config = '&image_type=photo&orientation=horizontal&per_page=12';
 
 export const getImages = (searchText, page) => {
   return fetch(`${BASE_URL}?q=${searchText}&page=${page}&key=${KEY}${config}`);
